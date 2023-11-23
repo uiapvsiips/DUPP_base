93676596527f52ae014b45a30c04b36e82d3a1e5
Kyrylo <Kyrylo@HOME-PC>
2023-11-23 18:44:15 +0200

    Initial commit

---
+++ windows/add_window.py
@@ -0,0 +1,14 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+
+
+class Add_Window(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Додати авто Укртрансбезпека")
+        self.geometry(f"{400}x{800}")
+        self.label = customtkinter.CTkLabel(self, text="Додати авто Укртрансбезпека").pack()
+        self.label2 = customtkinter.CTkLabel(self, text="Додати авто Укртрансбезпека1").pack()
+
