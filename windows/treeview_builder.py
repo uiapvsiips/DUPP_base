602204a69e6f39ee139f0b68651c61d8c47857c6
Kyrylo <uiapvsiips@gmail.com>
2023-11-26 21:23:01 +0200

    Начал добавлять описание к методам и классам

--- windows/treeview_builder.py
+++ windows/treeview_builder.py
@@ -13,16 +13,28 @@ from windows.utb_card_window import UTB_card_Window
 
 
 class TreeViewBuilder:
+    """
+    Класс для создания Treeview
+    :param root: корневой элемент, в котором создается таблица (вкладка)
+    :param master: главное окно приложения
+    """
+
     def __init__(self, root, master):
         self.master = master
-        self.tv = self.get_treeview_data(root)
-        self.yscrollbar = CTkScrollbar(self.tv, command=self.tv.yview)
-        self.yscrollbar.pack(side="right", fill="y")
-        self.yscrollbar.bind("<Enter>", self.mw_event)
-        self.tv.configure(yscrollcommand=self.yscrollbar.set)
-        self.add_info_to_table()
+        self.tv = self.get_treeview_data(root)  # создаем таблицу и заносим ее в переменную
+        self.yscrollbar = CTkScrollbar(self.tv, command=self.tv.yview)  # создаем скроллбар
+        self.yscrollbar.pack(side="right", fill="y")  # размещаем скроллбар справа
+        self.yscrollbar.bind("<Enter>", self.mw_event)  # привязываем событие прокрутки к скроллбару
+        self.tv.configure(yscrollcommand=self.yscrollbar.set)  # привязываем скроллбар к таблице
+        self.add_info_to_table()  # добавляем информацию в таблицу
 
     def get_treeview_data(self, root):
+        """
+        Функция для создания скелета таблицы
+        :param root:
+        :return:
+        """
+        # Назначаем стили
         bg_color = root._apply_appearance_mode(customtkinter.ThemeManager.theme["CTkFrame"]["fg_color"])
         text_color = root._apply_appearance_mode(customtkinter.ThemeManager.theme["CTkLabel"]["text_color"])
         selected_color = root._apply_appearance_mode(customtkinter.ThemeManager.theme["CTkButton"]["fg_color"])
@@ -33,8 +45,12 @@ class TreeViewBuilder:
                             borderwidth=0)
         treestyle.map('Treeview', background=[('selected', bg_color)], foreground=[('selected', selected_color)])
         root.bind("<<TreeviewSelect>>", lambda event: root.focus_set())
+
+        # создаем рамку
         frame_1 = customtkinter.CTkFrame(master=root)
         frame_1.pack(pady=0, padx=0, fill="both", expand=True)
+
+        # создаем таблицу со столбцами, как в базе данных
         treeview = ttk.Treeview(frame_1, show="headings",
                                 columns=("id", "number", "in_number", "car_going_date", "car_going_place",
                                          "car_info", "truck_info", "license_plate", "note",
@@ -42,77 +58,135 @@ class TreeViewBuilder:
                                 displaycolumns=("number", "in_number", "car_going_date", "car_going_place",
                                                 "car_info", "truck_info", "license_plate", "note",
                                                 "executor", "owner", "owner_phone"))
+        # добавляем столбцы в таблицу и задаем ширину столбцов, выравнивание и заголовки
+        # NUMBER
         treeview.heading("number", text="No", command=lambda: self.treeview_sort_column('number', True))
         treeview.column("number", width=5, anchor="center")
+
+        # IN_NUMBER
         treeview.heading("in_number", text="Вх.№",
                          command=lambda: self.treeview_sort_column('in_number', True))
         treeview.column("in_number", width=30, anchor="center")
+
+        # CAR_GOING_DATE
         treeview.heading("car_going_date", text="Дата та час проїзду",
                          command=lambda: self.treeview_sort_column('car_going_date', True))
         treeview.column("car_going_date", width=50, anchor="center")
+
+        # CAR_GOING_PLACE
         treeview.heading("car_going_place", text="Місце проїзду",
                          command=lambda: self.treeview_sort_column('car_going_place', True))
         treeview.column("car_going_place", width=100, anchor="center")
+
+        # CAR_INFO
         treeview.heading("car_info", text="Інфо про авто", command=lambda: self.treeview_sort_column('car_info', True))
         treeview.column("car_info", width=70, anchor="center")
+
+        # TRUCK_INFO
         treeview.heading("truck_info", text="Інфо про причеп",
                          command=lambda: self.treeview_sort_column('truck_info', True))
         treeview.column("truck_info", width=70, anchor="center")
+
+        # LICENSE_PLATE
         treeview.heading("license_plate", text="ДНЗ Оригінальний",
                          command=lambda: self.treeview_sort_column('license_plate', True))
         treeview.column("license_plate", width=40, anchor="center")
+
+        # NOTE
         treeview.heading("note", text="Примітка", command=lambda: self.treeview_sort_column('note', True))
         treeview.column("note", width=100, anchor="center")
+
+        # EXECUTOR
         treeview.heading("executor", text="Виконавець", command=lambda: self.treeview_sort_column('executor', True))
         treeview.column("executor", width=40, anchor="center")
+
+        # OWNER
         treeview.heading("owner", text="ПІБ Власника/Компанії",
                          command=lambda: self.treeview_sort_column('owner', True))
         treeview.column("owner", width=100, anchor="center")
+
+        # OWNER_PHONE
         treeview.heading("owner_phone", text="м.т.", command=lambda: self.treeview_sort_column('owner_phone', True))
         treeview.column("owner_phone", width=100, anchor="center")
+
+        # добавляем таблицу в окно, выравниваем и размещаем
         treeview.pack(fill="both", expand=True)
+
+        # привязываем событие двойного клика и событие прокрутки к скроллбару
         treeview.bind("<Double-1>", self.OnDoubleClick)
         treeview.bind("<MouseWheel>", self.mw_event)
         return treeview
 
     def mw_event(self, event):
+        """
+        Привязываем событие прокрутки к скроллбару и колесика мыши
+        :param event:
+        :return:
+        """
+        # если прокрутка вниз до последего элемента
         if self.tv.yview()[1] == 1.0:
+            # найти минимальный id
             min_id = min([d for d in [int(self.tv.set(k)['id']) for k in self.tv.get_children('')]])
-            # найти минимальный id
-            last_num = [(self.tv.set(k), k) for k in self.tv.get_children('')][-1][0]['number']
+            # найти последний номер
+            last_num = max([d for d in [int(self.tv.set(k)['number']) for k in self.tv.get_children('')]])
+            # добавить информацию
             self.add_info_to_table(int(min_id), int(last_num) + 1)
 
     def OnDoubleClick(self, event):
-        # Если двойной клик на заголовок
+        """
+        Обработка события двойного клика
+        :param event:
+        :return:
+        """
+
+        # Если клик на заголовок таблицы
         if event.widget.identify_region(event.x, event.y) == "heading":
+
+            # Получаем столбец, на который кликнули
             column_number = event.widget.identify_column(event.x).replace("#", "")
             column_name = event.widget["columns"][int(column_number)]
             heading_text = event.widget.heading(column_name, "text")
+
+            # Если клик по первому столбцу, то ничего не происходит
             if heading_text == "No":
                 return
+
+            # В ином случае открываем окно поиска. Если окно уже открыто, то просто фокусируем его
             if self.master.search_window is None or not self.master.search_window.winfo_exists():
                 self.master.search_window = SearchWindowBuilder(Utb, column_name, heading_text)
             else:
                 self.master.search_window.focus()
             self.master.search_window.after(100, self.master.search_window.search_entry.focus_force)
             self.master.search_window.wait_window()
+
+            # Если окно закрыто и какая-то информация была найдена, то обновляем таблицу
             if self.master.search_window.search_result:
                 self.tv.delete(*self.tv.get_children())
                 for search_result in self.master.search_window.search_result:
                     self.tv.insert("", "end", values=search_result)
             return
-            # Если двойной клик на элемент таблицы
+
+        #______________________________________________________________________________________________________________#
+
+        # Если двойной клик на элемент таблицы
         item_num = self.tv.selection()[0]
         item = self.tv.item(item_num, "values")
+
+        # Открываем сессию базы данных, находим запись, соответствующую выбранному элементу и выводим ее в новое
+        # окно
         with Session() as session:
             session: sqlalchemy.orm.Session
             qry = session.query(Utb).where(Utb.id == item[0])
             res = session.execute(qry)
             result = res.fetchone()
-            car_window = UTB_card_Window(mode='edit', info=result)
-            car_window.mainloop()
+            if self.master.add_car_window is None or not self.master.add_car_window.winfo_exists():
+                self.master.add_car_window = UTB_card_Window(mode='edit', info=result)
+            else:
+                self.master.add_car_window.focus()
+            self.master.add_car_window.in_number_entry.after(100, self.master.add_car_window.in_number_entry.focus_force)
 
     def treeview_sort_column(self, col, reverse):
+        #TODO продолжить добавлять описание к методам и классам treeview_builder, utb_card_window, serach_window, calendar_window
         if col == 'number':
             return
         l = [(self.tv.set(k, col), k) for k in self.tv.get_children('')]
