5b978e3424f0e5ef286e47f6f0f1c57ff8d09e1b
Kyrylo <Kyrylo@HOME-PC>
2023-11-29 10:44:21 +0200

    Привязка DB_URL к значению из конфига

--- db/engines/sync.py
+++ db/engines/sync.py
@@ -3,8 +3,9 @@ import sqlalchemy.orm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from config import db_url
 
-engine = create_engine('postgresql+psycopg2://stas:stas01@localhost:5432/uppdb', connect_args={"options": "-c timezone=utc-2"})
+engine = create_engine(db_url, connect_args={"options": "-c timezone=utc-2"})
 
 Session = sessionmaker(engine)
 
