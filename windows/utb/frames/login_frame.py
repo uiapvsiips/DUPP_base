41c24779d0bbb5979c283c4b7da14b9666eed305
Kyrylo <Kyrylo@HOME-PC>
2023-12-08 11:10:20 +0200

    login_frame

--- windows/utb/frames/login_frame.py
+++ windows/utb/frames/login_frame.py
@@ -1,4 +1,5 @@
 import os
+from copy import copy
 
 import customtkinter
 from CTkMessagebox import CTkMessagebox
@@ -7,6 +8,7 @@ from sqlalchemy import select
 import config
 from db.engines.sync import Session
 from db.models.user import User
+from windows.utb.frames.main_frame import MainFrame
 
 
 class LoginFrame:
@@ -37,16 +39,25 @@ class LoginFrame:
         if not login or not password:
             self.error_dialog = CTkMessagebox(master=self.root, title="Помилка", message="Заповніть всі поля")
             return
-        with Session() as session:
-            session: sqlalchemy.orm.Session
-            qry = select(User).where(User.username == login)
-            result = session.execute(qry)
-            result = result.scalars().first()
-            if result and result.password == password:
-                config.user = User(**dict(result))
-                self.root.login_frame.login_frame.grid_forget()
-                self.root.main_frame.grid(row=0, column=0, padx=(15, 20), pady=(10, 10), sticky="nsew")
-            else:
-                self.error_dialog = CTkMessagebox(master=self.root, title="Помилка",
-                                                  message="Невірний логін або пароль", )
-                print("")
+        qry = select(User).where(User.username == login)
+        result = config.session.execute(qry)
+        result = result.scalars().first()
+        if result and result.password == password:
+            result: User
+            config.user = copy(result)
+            self.root.login_frame.login_frame.grid_forget()
+
+            self.root.height = 780  # Высота окна
+            self.root.width = 1400  # Ширина окна
+            screen_width = self.root.winfo_screenwidth()
+            screen_height = self.root.winfo_screenheight()
+            x = (screen_width / 2) - (self.root.width / 2)
+            y = (screen_height / 2) - (self.root.height / 2)
+            self.root.geometry('%dx%d+%d+%d' % (self.root.width, self.root.height, x, y))
+
+            self.root.main_frame = MainFrame(self.root)
+            self.root.main_frame.grid(row=0, column=0, padx=(15, 20), pady=(10, 10), sticky="nsew")
+        else:
+            self.error_dialog = CTkMessagebox(master=self.root, title="Помилка",
+                                              message="Невірний логін або пароль", )
+            print("")
