190b9e032f10796ec1fb4ec5a832dae10a1b8d66
Kyrylo <Kyrylo@HOME-PC>
2023-12-08 11:11:43 +0200

    таблица с записями

--- windows/utb/treeview_builder.py
+++ windows/utb/treeview_builder.py
@@ -1,16 +1,21 @@
+from functools import partial
 from tkinter import ttk
+from tkinter.font import Font
 
+from CTkMessagebox import CTkMessagebox
 from sqlalchemy.orm import selectinload
 
 import customtkinter
 import sqlalchemy.orm
 from customtkinter import CTkScrollbar
 from sqlalchemy import select, desc
+from sqlalchemy.sql.functions import count
 
 import config
 from commonmethods import Utb_raw_to_list
 from config import show_mode
 from db.engines.sync import Session
+from db.models import Photo
 from db.models.utb_card import Utb
 from windows.utb.search_window import SearchWindowBuilder
 from windows.utb.utb_card_window import UTB_card_Window
@@ -26,11 +31,13 @@ class TreeViewBuilder:
     def __init__(self, root, master):
         super().__init__()
         self.master = master
+
         self.tv = self.get_treeview_data(root)  # создаем таблицу и заносим ее в переменную
         self.yscrollbar = CTkScrollbar(self.tv, command=self.tv.yview)  # создаем скроллбар
         self.yscrollbar.pack(side="right", fill="y")  # размещаем скроллбар справа
         self.yscrollbar.bind("<Enter>", self.mw_event)  # привязываем событие прокрутки к скроллбару
         self.tv.configure(yscrollcommand=self.yscrollbar.set)  # привязываем скроллбар к таблице
+        self.loading_bar = self.get_loading_bar(self.tv)
         self.add_info_to_table()  # добавляем информацию в таблицу
 
     def get_treeview_data(self, root):
@@ -47,16 +54,13 @@ class TreeViewBuilder:
         treestyle = ttk.Style()
         treestyle.theme_use('default')
         treestyle.configure("Treeview", background=bg_color, foreground=text_color, fieldbackground=bg_color,
-                            borderwidth=0)
+                            borderwidth=0, rowheight=45)
         treestyle.map('Treeview', background=[('selected', bg_color)], foreground=[('selected', selected_color)])
         root.bind("<<TreeviewSelect>>", lambda event: root.focus_set())
 
-        # создаем рамку
-        frame_1 = customtkinter.CTkFrame(master=root)
-        frame_1.pack(pady=0, padx=0, fill="both", expand=True)
 
         # создаем таблицу со столбцами, как в базе данных
-        treeview = ttk.Treeview(frame_1, show="headings",
+        treeview = ttk.Treeview(root, show="headings",
                                 columns=("id", "number", "in_number", "car_going_date", "car_going_place",
                                          "car_info", "truck_info", "license_plate", "note",
                                          "executor", "owner", "owner_phone"),
@@ -71,17 +75,17 @@ class TreeViewBuilder:
         # IN_NUMBER
         treeview.heading("in_number", text="Вх.№",
                          command=lambda: self.treeview_sort_column('in_number', True))
-        treeview.column("in_number", width=30, anchor="center")
+        treeview.column("in_number", width=10, anchor="center")
 
         # CAR_GOING_DATE
         treeview.heading("car_going_date", text="Дата та час проїзду",
                          command=lambda: self.treeview_sort_column('car_going_date', True))
-        treeview.column("car_going_date", width=50, anchor="center")
+        treeview.column("car_going_date", width=40, anchor="center")
 
         # CAR_GOING_PLACE
         treeview.heading("car_going_place", text="Місце проїзду",
                          command=lambda: self.treeview_sort_column('car_going_place', True))
-        treeview.column("car_going_place", width=100, anchor="center")
+        treeview.column("car_going_place", width=60, anchor="center")
 
         # CAR_INFO
         treeview.heading("car_info", text="Інфо про авто", command=lambda: self.treeview_sort_column('car_info', True))
@@ -99,7 +103,7 @@ class TreeViewBuilder:
 
         # NOTE
         treeview.heading("note", text="Примітка", command=lambda: self.treeview_sort_column('note', True))
-        treeview.column("note", width=100, anchor="center")
+        treeview.column("note", width=200, anchor="center")
 
         # EXECUTOR
         treeview.heading("executor", text="Виконавець", command=lambda: self.treeview_sort_column('executor', True))
@@ -115,7 +119,7 @@ class TreeViewBuilder:
         treeview.column("owner_phone", width=100, anchor="center")
 
         # добавляем таблицу в окно, выравниваем и размещаем
-        treeview.pack(fill="both", expand=True)
+        treeview.pack(side="left", fill="both", expand=True)
 
         # привязываем событие двойного клика и событие прокрутки к скроллбару
         treeview.bind("<Double-1>", self.OnDoubleClick)
@@ -180,19 +184,42 @@ class TreeViewBuilder:
 
         # Открываем сессию базы данных, находим запись, соответствующую выбранному элементу и выводим ее в новое
         # окно
-        with Session() as session:
-            session: sqlalchemy.orm.Session
-            qry = session.query(Utb).where(Utb.id == item[0]).options(selectinload(Utb.photos))
-            res = session.execute(qry)
-            result = res.scalar()
-            if config.add_car_window is None or not config.add_car_window.winfo_exists():
-                config.add_car_window = UTB_card_Window(mode='edit', info=result, item_num = item_num)
-            else:
-                config.add_car_window.focus()
-            config.add_car_window.in_number_entry.after(1, config.add_car_window.lift)
+        res = config.session.query(Utb).get(int(item[0]))
+        count_of_photos = config.session.query(count(Photo.id)).where(Photo.utb_id==res.id).scalar()
+        if config.add_car_window is None or not config.add_car_window.winfo_exists():
+            config.add_car_window = UTB_card_Window(mode='edit', info=res, count_of_photos=count_of_photos)
+        else:
+            config.add_car_window.focus()
+        config.add_car_window.in_number_entry.after(300, config.add_car_window.lift)
+        config.add_car_window.wait_window()
+        if config.add_car_window.row_for_delete:
+            self.delete_row_from_tv(item_num)
+        elif config.add_car_window.total_row:
+            self.edit_row_in_tv(item_num, config.add_car_window.total_row)
+        # try:
+        #     config.session.commit()
+        # except Exception as e:
+        #     print(f'Error: {e}')
+        #     config.session.rollback()
+        #     CTkMessagebox(title="Помилка\n", message=str(e))
 
+    def add_row_to_tv(self, row):
+        # Добавить запись в начало таблицы
+        self.tv.insert('', 0, values=row)
+        self.treeview_sort_column('id', True, only_numbers = True)
 
-    def treeview_sort_column(self, col, reverse):
+    def delete_row_from_tv(self, row):
+        # Удалить запись из таблицы
+        self.tv.delete(row)
+        self.treeview_sort_column('id', True)
+
+    def edit_row_in_tv(self, row_to_delete, row_to_add):
+        # Редактировать запись в таблице
+        self.tv.delete(row_to_delete)
+        self.tv.insert('', 0, values=row_to_add)
+        self.treeview_sort_column('id', True)
+
+    def treeview_sort_column(self, col, reverse, only_numbers = False):
         """
         Сортировка записей в таблице
         :param col:
@@ -215,6 +242,8 @@ class TreeViewBuilder:
             item[1] = index + 1
             self.tv.insert('', 'end', values=item)
 
+        self.loading_bar.pack_forget()
+
         # На следующий раз ставим сортировку в обратном порядке для текущего столбца
         self.tv.heading(col, command=lambda: self.treeview_sort_column(col, not reverse))
 
@@ -225,28 +254,34 @@ class TreeViewBuilder:
         :param last_num:
         :return:
         """
-        with Session() as session:
-            session: sqlalchemy.orm.Session
-            session.begin()
-            try:
-                # Если включен режим отображения всех записей
-                if show_mode == 'all':
-                    qry = select(Utb).where(Utb.id < last_id).order_by(desc(Utb.id)).limit(
-                        50) if last_id > 0 else select(Utb).where(Utb.id > last_id).order_by(desc(Utb.id)).limit(50)
-                # Если включен режим отображения записей после поиска
-                else:
-                    qry = config.last_qry.where(Utb.id < last_id).order_by(desc(Utb.id)).limit(50)
-                res = session.execute(qry)
-                lst = res.fetchall()
-                result = Utb_raw_to_list(lst, position=last_num)
-                for i in result:
-                    try:
-                        self.tv.insert('', 'end', values=i)
-                        last_num += 1
-                    except:
-                        print('Error:', i)
-            except Exception as e:
-                print(e)
-                session.rollback()
+        try:
+            # Если включен режим отображения всех записей
+            if config.show_mode == 'all':
+                qry = select(Utb).where(Utb.id < last_id).order_by(desc(Utb.id)).limit(
+                    50) if last_id > 0 else select(Utb).where(Utb.id > last_id).order_by(desc(Utb.id)).limit(50)
+            # Если включен режим отображения записей после поиска
             else:
-                session.commit()
+                qry = config.last_qry.where(Utb.id < last_id).order_by(desc(Utb.id)).limit(50)
+            res = config.session.execute(qry)
+            lst = res.fetchall()
+            result = Utb_raw_to_list(lst, position=last_num)
+            for i in result:
+                try:
+                    self.tv.insert('', 'end', values=i)
+                    last_num += 1
+                except:
+                    print('Error:', i)
+        except Exception as e:
+            print(e)
+
+    def get_loading_bar(self,root):
+        slider_progressbar_frame = customtkinter.CTkFrame(root, fg_color="transparent")
+        slider_progressbar_frame.pack(fill="both", expand=True)
+        slider_progressbar_frame.grid_columnconfigure(0, weight=1)
+        slider_progressbar_frame.grid_rowconfigure(0, weight=1)
+        progressbar_1 = customtkinter.CTkProgressBar(slider_progressbar_frame)
+        progressbar_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        progressbar_1.configure(mode='indeterminate')
+        progressbar_1.start()
+        slider_progressbar_frame.pack_forget()
+        return slider_progressbar_frame
