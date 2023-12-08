f067c2101538ffd9f9f16409d35ab71473eb54d0
Kyrylo <Kyrylo@HOME-PC>
2023-12-08 11:06:44 +0200

    main_window+main_frame

--- main_window.py
+++ main_window.py
@@ -1,7 +1,20 @@
+import atexit
+import os
+import sys
+from threading import Thread
+from time import sleep
+
 import customtkinter
 
+import config
+from commonmethods import get_all_users, get_data_for_auto_complete_utb, Utb_raw_to_list
+from db.engines.sync import Session
+from db.models import Utb
 from windows.utb.frames.login_frame import LoginFrame
-from windows.utb.frames.main_frame import MainFrame
+
+
+def get_last_id():
+    return config.session.query(Utb.id).order_by(Utb.id.desc()).limit(1).scalar()
 
 
 class MainWindow(customtkinter.CTk):
@@ -19,14 +32,50 @@ class MainWindow(customtkinter.CTk):
         y = (screen_height / 2) - (self.height / 2)
         self.geometry('%dx%d+%d+%d' % (self.width, self.height, x, y))
 
-        self.main_frame = MainFrame(self)
         self.login_frame = LoginFrame(self)
 
         self.login_frame.login_frame.grid(row=0, column=0, padx=(500), pady=(170), sticky="nsew")
         self.login_frame.login_frame.after(10, self.login_frame.login_entry.focus)
 
+        # self.main_frame = MainFrame(self)
+        # self.main_frame.grid(row=0, column=0, padx=(15, 20), pady=(10, 10), sticky="nsew")
+
+        # config.user = config.session.execute(select(User).where(User.username == 'www')).scalar()
+        thr = Thread(target=self.monitor_id)
+        thr.start()
+
+    def monitor_id(self):
+        while True:
+            if config.is_exit:
+                sys.exit()
+            id = get_last_id()
+            if config.last_id and id > config.last_id:
+                config.last_id = id
+                last_utb = config.session.query(Utb).get(id)
+                last_utb = Utb_raw_to_list(last_utb)
+                self.main_frame.tvb.loading_bar.pack(fill="both", expand=True)
+                self.main_frame.tvb.add_row_to_tv(last_utb)
+                self.main_frame.tvb.loading_bar.pack_forget()
+            sleep(5)
+
+
+def close_program():
+    config.session.commit()
+    config.session.close()
+    config.is_exit = True
+    print('Session was closed')
+    os._exit(0)
+
 
 if __name__ == "__main__":
-    customtkinter.set_appearance_mode("Dark")
-    app = MainWindow()
-    app.mainloop()
+    with Session() as session:
+        session.begin()
+        session: Session
+        config.session = session
+        config.last_id = get_last_id()
+        customtkinter.set_appearance_mode("Dark")
+        config.users = get_all_users()
+        config.data_for_complete_utb = get_data_for_auto_complete_utb()
+        app = MainWindow()
+        app.mainloop()
+        atexit.register(close_program)
--- windows/utb/frames/main_frame.py
+++ windows/utb/frames/main_frame.py
@@ -11,7 +11,7 @@ class MainFrame(customtkinter.CTkFrame):
         self.root = root
         # MainFrame на весь родительский контейнер
         self.configure(root, corner_radius=10, height=root.height - 20,
-                                                 width=root.width - 30)
+                       width=root.width - 30)
         self.grid_columnconfigure((0, 1, 2), weight=1)
         self.grid_rowconfigure(0, weight=20)
         self.grid_rowconfigure(1, weight=1)
@@ -20,32 +20,18 @@ class MainFrame(customtkinter.CTkFrame):
         self.tabview = customtkinter.CTkTabview(self, height=root.height - 60, width=root.width - 30,
                                                 corner_radius=20)
         self.tabview.add("Укртрансбезпека")
-        self.tabview.add("Інший облік")
-        self.tabview.grid(row=0, column=0, padx=(0), pady=(0), sticky="nsew", columnspan=3)
+        # self.tabview.add("Інший облік")
+        self.tabview.grid(row=0, column=0, padx=0, pady=0, sticky="nsew", columnspan=3)
 
         # Кнопки
         self.add_button = customtkinter.CTkButton(master=self, text="Додати", command=self.add_button_event)
-        self.add_button.grid(row=1, column=1, padx=(280), pady=(10), sticky="nsew")
+        self.add_button.grid(row=1, column=1, padx=280, pady=10, sticky="nsew")
 
         # Дерево(информация из таблицы)
+        self.tabview.tab("Укртрансбезпека").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Укртрансбезпека").grid_rowconfigure(0, weight=1)
         self.tvb = TreeViewBuilder(self.tabview.tab("Укртрансбезпека"), self)
         self.tv = self.tvb.tv
-        self.tv.pack(fill="both", expand=True)
-
-    def add_row_to_tv(self, row):
-        # Добавить запись в начало таблицы
-        self.tv.insert('', 0, values=row)
-        self.tvb.treeview_sort_column('id', True)
-
-    def delete_row_from_tv(self, row):
-        # Удалить запись из таблицы
-        self.tv.delete(row)
-        self.tvb.treeview_sort_column('id', True)
-
-    def edit_row_in_tv(self, row_to_delete, row_to_add):
-        self.tv.delete(row_to_delete)
-        self.tv.insert('', 0, values=row_to_add)
-        self.tvb.treeview_sort_column('id', True)
 
     def add_button_event(self):
         """
@@ -59,6 +45,14 @@ class MainFrame(customtkinter.CTkFrame):
                 config.add_car_window = UTB_card_Window()
             else:
                 config.add_car_window.focus()
-            config.add_car_window.in_number_entry.after(200, config.add_car_window.in_number_entry.focus_force)
+            config.add_car_window.in_number_entry.after(300, config.add_car_window.in_number_entry.focus_force)
+            config.add_car_window.wait_window()
+            if config.add_car_window.total_row:
+                self.tvb.add_row_to_tv(config.add_car_window.total_row)
         elif current_tab == "Інший облік":
             pass
+
+
+if __name__ == '__main__':
+    app = MainFrame()
+    app.mainloop()
