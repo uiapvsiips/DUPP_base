8bb6f1533722ca1ee03a3c6a7bbbd5e55ec0cea7
Kyrylo <Kyrylo@HOME-PC>
2023-12-08 10:52:02 +0200

    Обновление config файлов

--- config.py
+++ config.py
@@ -1,20 +1,40 @@
+import configparser
 import os
+import sys
 
-from commonmethods import get_all_users, get_data_for_auto_complete_utb
-from db.models.user import User
+from aiogram import Bot, Dispatcher
 
-user: None | User = None  # Текущий пользователь программы
-users: list = get_all_users()  # Список всех пользователей из базы
-data_for_complete_utb = get_data_for_auto_complete_utb()  # Данные для автозаполнения
+is_exit = False
+last_id = None
+session = None
+user: None = None  # Текущий пользователь программы
+users = None  # Список всех пользователей из базы
+data_for_complete_utb = None  # Данные для автозаполнения
 show_mode = 'all'  # Режим отображения
 last_qry = None  # Последний запрос
 add_car_window = None  # Окно добавления авто
 search_window = None  # Окно поиска
 
-__db_username = os.environ.get('DB_USERNAME', 'stas')
-__dp_password = os.environ.get('DB_PASSWORD', 'stas01')
-__db_name = os.environ.get('DB_NAME', 'dupp')
-__db_host = os.environ.get('DB_HOST', 'localhost')
-__db_port = os.environ.get('DB_PORT', '5432')
+is_postgres = os.environ.get('TELEGRAM_BOT_TOKEN', None)
 
-db_url = f"postgresql+psycopg2://{__db_username}:{__dp_password}@{__db_host}:{__db_port}/{__db_name}"
+BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '6379535978:AAGHlgXh2rWLj91ZWbd-OUEokO6xljX5Zjc')
+bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher()
+
+# __db_username = os.environ.get('DB_USERNAME', 'stas')
+# __dp_password = os.environ.get('DB_PASSWORD', 'stas01')
+# __db_name = os.environ.get('DB_NAME', 'uppdb')
+# __db_host = os.environ.get('DB_HOST', '101.4.6.68')
+# __db_port = os.environ.get('DB_PORT', '5432')
+
+
+if not is_postgres:
+    # abs_config_path = os.path.abspath(os.path.dirname(sys.executable))+'\\config.ini' # путь для компиляции EXE файла
+    abs_config_path = os.path.dirname(os.path.abspath(__file__)) + '\\config.ini'
+    config = configparser.ConfigParser()
+    config.read(abs_config_path)
+
+    sync_db_url = os.environ.get('DATABASE_URL', f"postgresql+psycopg2://{config.get('PG', 'db_url')}")
+    async_db_url = os.environ.get('ASYNC_DATABASE_URL', f"postgresql+asyncpg://{config.get('PG', 'db_url')}")
+else:
+    async_db_url = os.environ.get('ASYNC_DATABASE_URL')
